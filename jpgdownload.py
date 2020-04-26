a04ee6742f243eca81682a63125b1087a1d84f30
J.J.Ding <louis7012161@gmail.com>
2020-04-26 23:40:50 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Update

--- jpgdownload.py
+++ jpgdownload.py
@@ -1,1 +1,8 @@
-url="https://megapx-assets.dcard.tw/images/8ebedc15-76dc-4a27-92f0-0807583c229e/640.jpeg"
+import urllib.request
+
+
+url_pict="https://megapx-assets.dcard.tw/images/8ebedc15-76dc-4a27-92f0-0807583c229e/640.jpeg"
+fn = "sexy_pict.jpg"
+pict = urllib.request.urlretrieve(url_pict,fn)
+
+##Result:urllib.error.HTTPError: HTTP Error 403: Forbidden
