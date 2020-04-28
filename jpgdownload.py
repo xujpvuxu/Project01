caa006f63dad5dd0aed2fdc81df27474544a6910
xujpvuxu <xujpvuxu2332@gmail.com>
2020-04-28 23:19:19 +0800

    Signed-off-by: xujpvuxu <xujpvuxu2332@gmail.com> add GUI 99Kubo.py

--- jpgdownload.py
+++ jpgdownload.py
@@ -1,1 +1,3 @@
-url="https://megapx-assets.dcard.tw/images/8ebedc15-76dc-4a27-92f0-0807583c229e/640.jpeg"
+import urllib.request
+url="http://www.dudu-sex.com/images/vape450.jpg"
+urllib.request.urlretrieve(url)
