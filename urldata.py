566139c8749a358cd15b20dcb65140bde0ac9030
xujpvuxu <xujpvuxu2332@gmail.com>
2020-04-29 22:55:46 +0800

    cancel class

--- urldata.py
+++ urldata.py
@@ -2,28 +2,28 @@ import urllib.request as req
 import bs4
 
 
-class crawler():
-    def urldatau(self, url):
-        request = req.Request(url, headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
-        })
 
-        with req.urlopen(request) as response:
-            data = response.read().decode("utf-8")
+def urldatau(self, url):
+    request = req.Request(url, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    })
 
-        root = bs4.BeautifulSoup(data, "html.parser")
+    with req.urlopen(request) as response:
+        data = response.read().decode("utf-8")
 
-        return root
+    root = bs4.BeautifulSoup(data, "html.parser")
 
-    def urldatab(self, url):
-        request = req.Request(url, headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
-        })
+    return root
 
-        with req.urlopen(request) as response:
-            data = response.read().decode("BIG5")
+def urldatab(self, url):
+    request = req.Request(url, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    })
 
-        import bs4
-        root = bs4.BeautifulSoup(data, "html.parser")
+    with req.urlopen(request) as response:
+        data = response.read().decode("BIG5")
 
-        return root
+    import bs4
+    root = bs4.BeautifulSoup(data, "html.parser")
+
+    return root
