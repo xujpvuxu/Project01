769ebe24882f9345d56c07c0833ee56bc2d57af3
xujpvuxu <xujpvuxu2332@gmail.com>
2020-05-07 17:16:39 +0800

    Signed-off-by: xujpvuxu <xujpvuxu2332@gmail.com> download facebook video

---
+++ facebook.py
@@ -0,0 +1,17 @@
+import urldata,re
+import urllib.request
+
+url=input("網址:")
+data=urldata.urldatau(url)
+
+line = data.find(text=re.compile('src_no'))
+
+regex= re.compile('src_no_ratelimit:".*?"')
+url= regex.findall(line)
+
+reg = re.compile('".*?"')
+url=reg.findall(url[0])
+
+stru=str(url[0]).replace('"','')
+urllib.request.urlretrieve(stru,"1.mp4")
+
