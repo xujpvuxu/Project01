7a8440f131d26fb1b17a2a6b3624fee55319a640
xujpvuxu <xujpvuxu2332@gmail.com>
2020-05-07 21:51:00 +0800

    add

--- 筆試題目.py
+++ 筆試題目.py
@@ -1,34 +1,22 @@
 import urllib.request as req
 import urldata
 
-
-
 url="https://rate.bot.com.tw/xrt/flcsv/0/day"
 
 req.urlretrieve(url,"每日匯率.csv")
-
-
 
 data= open("每日匯率.csv","r",encoding="utf-8")
 wdata= open("JPY匯率.txt","w",encoding="utf-8")
 
 for i in range(20):
-
     line=data.readline()
-    
-
     if i == 0 :
         global linez
         linez=line.split(",")
- 
-    if "JPY" in line:
-
+     if "JPY" in line:
         linelist = line.split(",")
         for i in range(4):
-
             wdata.write(linez[i]+":" +linelist[i]+"\n")
-            
-
 data.close()
 wdata.close()
             
