caaa41c33af5942aee2b1a8140066d7c6442b524
xujpvuxu <xujpvuxu2332@gmail.com>
2020-05-12 17:07:05 +0800

    Signed-off-by: xujpvuxu <xujpvuxu2332@gmail.com> fix code

--- dcardsex.py
+++ dcardsex.py
@@ -1,5 +1,8 @@
 import urldata,re
+import webbrowser
 url="https://www.dcard.tw/f/sex"
+
+bo=True
 
 maindata=urldata.urldatau(url)
 divisaf=maindata.select('a.sc-1v1d5rx-3')
@@ -13,24 +16,26 @@ for count in range(len(divisaf)):
         for a in range(len(pichrefppt)):
             if a == 0 :
                 out.write(url+"\n")
+                webbrowser.open(url)
                 out.write(pichrefppt[a].string+"\n")
+                bo=False
             else:
                 out.write(pichrefppt[a].string+"\n")
 
         pichrefrisu = benchsaf.find_all('a',{'href':re.compile('risu')})
         for a in range(len(pichrefrisu)):
             if a == 0 :
-                out.write(url+"\n")
+                if bo == True:
+                    out.write(url+"\n")
+                    webbrowser.open(url)
                 out.write(pichrefrisu[a].string+"\n")
             else:
                 out.write(pichrefrisu[a].string+"\n")
+        bo=True
 out.close()
                    
 
 # picline = benchsaf.find(text=re.compile('props'))
 # regex = re.compile('https://risu.*?')
 # match=regex.findall(picline)
-#123
 
-#Hello World
-#Hello World2
